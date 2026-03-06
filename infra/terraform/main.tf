b7c325f2bf9173db7d9d2504541e5af38c13ee8a
Michael Hollander <mh.next@gmail.com>
2026-03-06 14:52:25 -0500

    Fix Terraform 1.5 compatibility and ACM validation mapping

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -129,25 +129,17 @@ resource "aws_acm_certificate" "api" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.api.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
   zone_id         = data.aws_route53_zone.selected.zone_id
-  name            = each.value.name
-  type            = each.value.type
-  records         = [each.value.record]
+  name            = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_name
+  type            = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_type
+  records         = [tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_value]
   ttl             = 60
   allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "api" {
   certificate_arn         = aws_acm_certificate.api.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
 
 resource "aws_apigatewayv2_domain_name" "api" {
